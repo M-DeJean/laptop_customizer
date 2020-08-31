7420be2d5523ba84f67c2bc27c4746ae4b9af2d3
Malik DeJean <mdejean@MacBook-Pro.local>
2020-08-30 17:40:34 -0700

    Updates

---
+++ precache-manifest.9fc219b30f36dcfc8f83d9cc96dde2bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278f980a269f06b7d0cc1d5a78c89f04",
+    "url": "/laptop_customizer/index.html"
+  },
+  {
+    "revision": "b03c91067c7da2102c7f",
+    "url": "/laptop_customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/laptop_customizer/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "b03c91067c7da2102c7f",
+    "url": "/laptop_customizer/static/js/main.21e16266.chunk.js"
+  },
+  {
+    "revision": "54eb8c3b7e85a3776709",
+    "url": "/laptop_customizer/static/js/runtime~main.11bd55f9.js"
+  }
+]);
